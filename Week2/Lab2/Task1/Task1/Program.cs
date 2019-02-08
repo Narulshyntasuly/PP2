e07b6bb50bacb633bf0695c46a5fd1b074c91ec0
Narul <2gc200006@gmail.com>
2019-02-09 02:14:15 +0600

    Lab2

---
+++ Week2/Lab2/Task1/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public static bool Palindrom(string pal) // метод проверяющий палиндромность
+        {
+            for (int i = 0; i < pal.Length; i++) 
+            {
+                if (pal[i] != pal[pal.Length - i - 1]) // проверерка
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+            
+        static void Main(string[] args)
+        {
+            string pal = Console.ReadLine(); 
+            if (!Palindrom(pal))    // проверка строки методом палиндром
+            {
+                Console.WriteLine("NO");
+
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
